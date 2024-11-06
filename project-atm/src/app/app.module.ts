8e6baa1388adf60d3b36f98ba0e14bb2dfcc4096
Muhammad Denny Setyawan <147497783+010Arthur010@users.noreply.github.com>
2024-11-06 08:22:20 +0700

    add-component-main-page

--- project-atm/src/app/app.module.ts
+++ project-atm/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
