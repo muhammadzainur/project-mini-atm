0cb803bce2d372b4020542b03ad5c6d9341516f8
Muhammad Denny Setyawan <147497783+010Arthur010@users.noreply.github.com>
2024-11-12 10:18:32 +0700

    add cek saldo

--- project-atm/src/app/app.module.ts
+++ project-atm/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { CekSaldoComponent } from './cek-saldo/cek-saldo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    CekSaldoComponent
   ],
   imports: [
     BrowserModule,
