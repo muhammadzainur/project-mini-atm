af928e753fe5d39ab22be97f8dbd98fe74e6a8e8
Muhammad Zainur <muhammadzainal2194@gmail.com>
2024-11-12 10:14:26 +0700

    input pin and routing

--- project-atm/src/app/app-routing.module.ts
+++ project-atm/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputPinComponent } from './input-pin/input-pin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'input-pin', component: InputPinComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- project-atm/src/app/app.module.ts
+++ project-atm/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { InputPinComponent } from './input-pin/input-pin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    InputPinComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
