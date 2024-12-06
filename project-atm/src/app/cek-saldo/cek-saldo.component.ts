d6b752b34ffdfd620434423f747bd726dcf6ddde
Muhammad Zainur <muhammadzainal2194@gmail.com>
2024-12-06 08:19:46 +0700

    fix-layout

--- project-atm/src/app/cek-saldo/cek-saldo.component.ts
+++ project-atm/src/app/cek-saldo/cek-saldo.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cek-saldo',
   templateUrl: './cek-saldo.component.html',
-  styleUrl: './cek-saldo.component.css'
+  styleUrl: './cek-saldo.component.css',
 })
 export class CekSaldoComponent {
+  isPopupVisible: boolean = false;
 
+  constructor(private router: Router) {}
+
+  showPopup(): void {
+    this.isPopupVisible = true;
+  }
+
+  hidePopup(): void {
+    this.isPopupVisible = false;
+  }
+
+  confirmExit(): void {
+    this.isPopupVisible = false;
+
+    // Navigasi ke halaman Terima Kasih
+    this.router.navigate(['/thank-you']);
+  }
 }
--- project-atm/src/app/thank-you/thank-you.component.ts
+++ project-atm/src/app/thank-you/thank-you.component.ts
@@ -10,9 +10,8 @@ export class ThankYouComponent implements OnInit {
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    // Tunggu selama 3 detik sebelum navigasi ke dashboard
     setTimeout(() => {
-      this.router.navigate(['/dashboard']);
-    }, 3000); // 3000ms = 3 detik
+      this.router.navigate(['']);
+    }, 2000);
   }
 }
